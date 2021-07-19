2fd8b0c9f39620ea27de450e5982d28b1a140f5e
aecanales <aecanales@uc.cl>
2021-07-19 11:27:30 -0400

    :clapper: :scroll: Add second hand

--- 03 - Annotations and Notifications/Assets/Clock.cs
+++ 03 - Annotations and Notifications/Assets/Clock.cs
@@ -5,6 +5,7 @@ public class Clock : MonoBehaviour
 {
     public GameObject HourHand;
     public GameObject MinuteHand;
+    public GameObject SecondHand;
     
     void Update()
     {
@@ -14,21 +15,24 @@ public class Clock : MonoBehaviour
         float hour = float.Parse(now.ToString("%h"));
         float minute = (float) now.Minute;
         float second = (float) now.Second;
+        float millisecond = (float) now.Millisecond;
 
         float smoothHour = hour + minute / 60;
         float smoothMinute = minute + second / 60;
+        float smoothSecond = second + millisecond / 1000;
 
         float hourAngle = getAngle(1, 12, smoothHour);
         float minuteAngle = getAngle(0, 60, smoothMinute);
+        float secondAngle = getAngle(0, 60, smoothSecond);
 
         HourHand.transform.rotation = Quaternion.Euler(0, 0, hourAngle);
         MinuteHand.transform.rotation = Quaternion.Euler(0, 0, minuteAngle);
+        SecondHand.transform.rotation = Quaternion.Euler(0, 0, secondAngle);
     }
 
     float getAngle(float min, float max, float value)
     {
         float lerp = Mathf.InverseLerp(min, max, value);
-
         return Mathf.Lerp(360, 0, lerp);
     }
 }
