6375b49d2415f80c7575d78c311ec317e6a7ca29
Alonso Canales <aecanales@uc.cl>
2021-08-09 12:58:25 -0400

    :scroll: :clapper: Change movement script and add bugs to scene

--- 04 - Events and Commands/Assets/Bug.cs
+++ 04 - Events and Commands/Assets/Bug.cs
@@ -1,21 +1,26 @@
 using UnityEngine;
 
+// Script that moves the bug randomly and returns its collision box.
 public class Bug : MonoBehaviour 
 {
-    public Vector2 MovementVector;
-    
     private Vector3 startingPosition;
     private Vector3 finalPosition;
+    
+    private float speedModifier;
 
     void Start()
     {
+        // We use Random to calculate the final position and a speed modifier.
         startingPosition = transform.position;
-        finalPosition = startingPosition + (Vector3) MovementVector;
+        finalPosition = startingPosition + new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-1.5f, 1.5f));
+        
+        speedModifier = Random.Range(0.5f, 1.5f);
     }
 
     void Update()
     {
-        float pingPong = Mathf.PingPong(Time.time, 1);
+        // We use ping-pong and lerp to move between the two positions.
+        float pingPong = Mathf.PingPong(Time.time * speedModifier, 1);
         transform.position = Vector3.Lerp(startingPosition, finalPosition, pingPong);
     }
 }
